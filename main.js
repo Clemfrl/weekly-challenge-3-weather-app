861dad9cf58e2b63c2fabd8aa962715422df4704
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2021-05-25 14:30:57 +0200

    Displaying : temperature , weather , high and low temp

--- main.js
+++ main.js
@@ -29,3 +29,14 @@ function displayResults(weather) {
   let date = document.querySelector(".location .date");
   date.innerText = dateBuilder(now);
 
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector(".current .weather");
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
+    weather.main.temp_max
+  )}°c`;
+}
