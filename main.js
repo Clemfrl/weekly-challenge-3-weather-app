b6bc5a702f21c2a87e9a53e90617cde1840f2dcc
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2021-05-25 13:50:56 +0200

    made the searchbox useable

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const api = {
   key: "633b3f3f60940266b254b5a5b998e5e1",
   base: "https://api.openweathermap.org/data/2.5/",
 };
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
